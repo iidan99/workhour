22f7119aa8b71ba01f2723b744f8e00c526d1239
idanhershman <idanh@herolo.co.il>
2019-05-07 09:04:10 +0300

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBR5c23qkCNfgHlTvFkLVV2cDumY3X05Io",
+    authDomain: "workhour-5c1b5.firebaseapp.com",
+    databaseURL: "https://workhour-5c1b5.firebaseio.com",
+    projectId: "workhour-5c1b5",
+    storageBucket: "workhour-5c1b5.appspot.com",
+    messagingSenderId: "813888053688",
+    appId: "1:813888053688:web:a7f7e2c6499f6042"
+  }
 };
